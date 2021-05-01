affba49e26f9eae57865e28d07fd6eb0bc90f492
pbraer <73088589+pbraer@users.noreply.github.com>
2021-05-02 00:58:05 +0700

    head

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hi')
+# Case-study #8-2
+# Developers:   Braer P. (%),
+#               Kokorina D. (%),
+#               Novoselov V. (%)
+
+print("""Case-study Файловая система
+Разработчики:
+Браер П.С., Кокорина Д.Е., Новоселов В.В.
+""")
